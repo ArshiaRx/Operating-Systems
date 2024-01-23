a689eb7cd236a266f440a4388e541cc1f3c670cf
Arshia RM <82693306+ArshiaRx@users.noreply.github.com>
2024-01-23 14:55:41 -0500

    Update pri_queue.c

--- Lab1/pri_queue.c
+++ Lab1/pri_queue.c
@@ -7,70 +7,39 @@ static Node_ptr_t head = NULL;
  * Insert a Node into a priority queue.
  * @param priority
  * @param data
- * @author Arshia Rahim
+ * @author Arshia Rahim - 500994106
  */
 
 void PQ_insert(int priority, char * data) {
  //FIX THIS
 
  //Create new node to insert
- Node_ptr_t newNode = malloc(sizeof(Node_t));
- newNode-> priority = priority;
- newNode-> data = data;
- newNode-> next = NULL;
-
- // Create Temporary node to use
- Node_ptr_t temp = malloc(sizeof(Node_t));
- Node_ptr_t curr = malloc(sizeof(Node_t));
- Node_ptr_t NodeInsert = malloc(sizeof(Node_t));
-
- // Check if head is NULL, if yes then insert node as head
+ Node_ptr_t newNode, newTemp;
+ newNode = (Node_ptr_t)malloc(sizeof(Node_t));
+ newNode -> data = data;
+ newNode -> priority = priority;
+ 
+ //Check if the first head node is empty
  if (head == NULL){
-     head = malloc(sizeof(Node_t));
-     NodeInsert =  newNode;
+     newNode -> next = NULL;
+     head = newNode; 
+ } 
+ 
+ //Check if newNode_priority greater than head, swap if so
+ else if (head-> priority <  newNode->priority){
+     newNode->next = head;
+     head = newNode;
  }
-
- //If the priority of new node is greater than the currect head's priority
- //Then push new node to head, and previous head moves back to PQ
- //First store old head onto temp code
+ 
+ // Else do the following... 
  else {
-    if (priority > head->priority){
-        temp->data = head->data;
-        temp->priority = head->priority;
-        temp->next = temp->next;
-
-    //Make new node the head, and make the next node the previous head
-    //Which was stored in temp node
-        head = newNode;
-        head->next = temp;
-
-    //Traverse list until you reach the end, or find a node with
-    //a lower priority than the new node
-        }
-        else {
-            NodeInsert = newNode;
-            curr = head;
-            while(curr -> next != NULL & NodeInsert -> priority < curr->next->priority){
-                curr = curr->next;
-            }
-
-    //Insert at the end of the PQ
-
-            if (curr->next == NULL){
-                curr->next = NodeInsert;
-                NodeInsert->next = NULL;
-            }
-
-    //Insert in the middle of PQ (If new node's PQ higher than an existing node's PQ)
-            else {
-                temp = malloc(sizeof(Node_t));
-                temp = curr->next;
-                curr->next = NodeInsert;
-                NodeInsert->next = temp;
-            }
-        }
+     for (newTemp = head; newTemp-> next->next != NULL && newNode->priority  < newTemp->priority; newTemp = newTemp->next){
+         
+     }
+     newNode->next = newTemp->next;
+     newTemp->next = newNode;
     }
- }
+}
 
 /**
  * Delete and return the node with the highest priority.
@@ -80,13 +49,11 @@ Node_ptr_t PQ_delete() {
   //FIX THIS
 
   //Create a new temp
-  Node_ptr_t temp = malloc(sizeof(Node_t));
+  Node_ptr_t temp = head;
 
-  temp = head;
   head = head->next;
-  free(temp);
 
-    return NULL;
+    return temp;
 }
 
 /**
@@ -107,5 +74,3 @@ int PQ_get_size() {
     for(tmp = head; tmp != NULL; tmp = tmp->next, size++);
     return size;
 }
-
-
