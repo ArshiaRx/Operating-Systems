831a22174c047c23d88f4b853817de473674b77f
Arshia RM <82693306+ArshiaRx@users.noreply.github.com>
2024-02-27 16:09:27 -0500

    Add files via upload

---
+++ lab5/lab5main.c
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   lab5main.c
+ * Author: a4rahim
+ */
+
+#include  <stdio.h> /* printf */
+#include  <sys/types.h> /* pid_t */
+#include <unistd.h> /* get_pid */
+#include <stdlib.h>     /* exit, EXIT_FAILURE */
+#include <sys/wait.h>  /* wait */
+#include <pthread.h>
+#include <sys/stat.h>
+
+void * count(void *);
+int globalNumber = 0;
+//Create a mutex
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER; 
+
+int main(void) {
+    int i;
+    pthread_t counterThread[5];
+
+    //Create the 5 threads
+    //Wait for all treads to finish
+    
+    for (i = 0; i < 5; i++){
+        pthread_create(&counterThread[i], NULL, count, (void*)NULL);
+        pthread_join(counterThread[i], NULL);
+    }
+    
+    printf("End Global Number: %d\n", globalNumber);
+    return (0);
+}
+
+void * count(void * junk) {
+    int loopCount = 0;
+    pthread_mutex_lock(&mutex1);
+    while (loopCount < 10) {
+        int tmpNumber = globalNumber;
+        printf("counter: %d, Thread: %ld, PID: %d\n",
+                tmpNumber, pthread_self(), getpid());
+        tmpNumber = globalNumber;
+        tmpNumber++;
+        usleep(random() % 2);
+        globalNumber = tmpNumber;
+        loopCount++;
+    }
+    pthread_mutex_unlock(&mutex1);
+    return (0);
+}
